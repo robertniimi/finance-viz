2087f67e8d5f38650ae24b188f9a88a93617a21b
Robert Niimi <robertn702@gmail.com>
2015-12-12 16:14:06 -0800

    removes datafiles from github

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+var $ = require('gulp-load-plugins')();
+
+// PATHS
+
+gulp.task('default', () => {
+
+});
+
+gulp.task('serve', () => {
+
+});
